56f0a08f02e29ac9740a9823269e3d2a6c5ecd3b
islam <islam@gmail.com>
2020-05-08 21:12:13 +0200

    second commit

--- src/find.java
+++ src/find.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class find {
 
     private int[] id;
+    private int[] sz;
     public find(int N){
         id = new int[N];
+        sz = new int[N];
+        Arrays.fill(sz, 1);
         for (int i = 0; i< N; i++)
             id[i] = i;
     }
@@ -27,6 +32,22 @@ public class find {
         int root_a = root(a);
         int root_b = root(b);
         id[a] = root_b;
+
+    }
+    public int size_of_tree( int a){
+        int i=1;
+        while( a != id[a]) i++;
+        return i;
+    }
+
+    // method for reducing the length of tree. This method will put short tree under long one.
+    public void union_by_weighted( int a , int b){
+        int root_a = root (a);
+        int root_b = root (b);
+
+
+        if  ( sz[root_a]< sz[root_b]) { id[root_a] = b; sz[root_b] += sz[root_a]; }
+        else   { id[root_b] = root_a; sz[root_a] += sz[root_b]; }
 
     }
     public static void main( String[] args)
